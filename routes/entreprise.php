c663c20a6f3f06c3342c6e0f0ae95be4e59d0b8e
Abdenour Garti <garti.abdenour@gmail.com>
2025-04-12 23:29:37 +0100

    change routes

--- routes/entreprise.php
+++ routes/entreprise.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/entreprises', [EntrepriseController::class, 'index'])->name('entreprises.index');
 Route::get('/entresprise/{entreprise}', [EntrepriseController::class, 'show'])->name('entreprises.show');
 
-Route::middleware(['auth', 'entreprise'])->prefix('entreprise')->name('entreprise.')->group(function () {
-    Route::get('/dashboard', [EntrepriseController::class, 'dashboard'])->name('dashboard');
-    Route::get('/profile', [EntrepriseController::class, 'profile'])->name('profile');
-    Route::put('/profile', [EntrepriseController::class, 'updateProfile'])->name('profile.update');
-    
+Route::middleware(['auth', 'entreprise'])->prefix('entreprise')->group(function () {
+    Route::get('/dashboard', [EntrepriseController::class, 'dashboard'])->name('entreprise.dashboard');
+    Route::get('/profile', [EntrepriseController::class, 'profile'])->name('entreprise.profile');
+    Route::put('/profile', [EntrepriseController::class, 'updateProfile'])->name('entreprise.profile.update');
     // Les technologies seront gérées directement dans le contrôleur EntrepriseController
 });
+
--- routes/offre.php
+++ routes/offre.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Entreprise\EntrepriseController;
 use App\Http\Controllers\Offre\OffreEmploiController;
 use App\Http\Controllers\Offre\ApplicationController;
 use Illuminate\Support\Facades\Route;
@@ -8,13 +9,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/offres', [OffreEmploiController::class, 'index'])->name('offres.index');
 Route::get('/offres/{offre}', [OffreEmploiController::class, 'show'])->name('offres.show');
 // Routes pour les entreprises
-Route::middleware(['auth', 'entreprise'])->prefix('entreprise')->name('entreprise.')->group(function () {
+Route::middleware(['auth', 'entreprise'])->prefix('entreprise')->group(function () {
 
     Route::resource('offres', OffreEmploiController::class)->except(['index', 'show']);
-    Route::get('/mes-offres', [OffreEmploiController::class, 'mesOffres'])->name('mes-offres');
-    Route::get('/offres/{offre}/applications', [ApplicationController::class, 'index'])->name('offres.applications.index');
-    Route::get('/offres/{offre}/applications/{application}', [ApplicationController::class, 'show'])->name('offres.applications.show');
+    Route::post('/post/offre', [OffreEmploiController::class, 'store'])->name('entreprise.offres.store');
+    Route::get('/mes-offres', [OffreEmploiController::class, 'mesOffres'])->name('entreprise.mes-offres');
+    Route::get('/offres/{offre}/applications', [ApplicationController::class, 'index'])->name('entreprise.offres.applications.index');
+    Route::get('/offres/{offre}/applications/{application}', [ApplicationController::class, 'show'])->name('entreprise.offres.applications.show');
 
+    Route::get('/ajouterOffre', [EntrepriseController::class, 'addOffre'])->name('entreprise.addOffre');
 
 
 });
